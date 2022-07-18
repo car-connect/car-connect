b657f51576839cb8ec2ed3c94b777206b4b76e93
anugrahs123 <anugrahsasikumar@gmail.com>
2022-07-18 16:21:15 +0530

    first commit

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(public http:HttpClient, public userService:UserService) { }
+
+  User:any={
+    username:'',
+    email:'',
+    password:''
+  }
+  addUser(){
+    this.userService.UserADD(this.User)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(public http:HttpClient,public router:Router) { }
+
+  UserADD(item:any){
+    this.http.post(`http://localhost:8000/user`,item)
+    .subscribe((res)=>{
+      this.router.navigate(['login'])
+      console.log("yeh",res);
+      
+    })
+    
+    
+      
+    
+  }
+}
